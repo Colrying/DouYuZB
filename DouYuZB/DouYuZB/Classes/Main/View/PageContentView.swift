88ab55403296d2627dbe3117028c3eb0e8082642
colrying <123792055@qq.com>
2017-04-12 16:25:55 +0800

    首页分组页面的逻辑实现

---
+++ DouYuZB/DouYuZB/Classes/Main/View/PageContentView.swift
@@ -0,0 +1,21 @@
+//
+//  PageContentView.swift
+//  DouYuZB
+//
+//  Created by 皇坤鹏 on 17/4/12.
+//  Copyright © 2017年 皇坤鹏. All rights reserved.
+//
+
+import UIKit
+
+class PageContentView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
